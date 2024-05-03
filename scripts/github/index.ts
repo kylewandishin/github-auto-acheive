791d026e0d1f6cb789243d7115b2d3dce052e1a6
Kyle W <73055576+dishman1805@users.noreply.github.com>
2024-05-02 23:14:26 -0600

    restyle code

--- scripts/github/index.ts
+++ scripts/github/index.ts
@@ -19,7 +19,7 @@ export class GithubClient {
     this.#browser = new PlaywrightBrowser();
   }
 
-  private validateCredentials(): void{
+  private validateCredentials(): void {
     try {
       if (
         this.#GIT_USER === '__YOUR_GITHUB_USERNAME__' ||
@@ -38,19 +38,19 @@ export class GithubClient {
     }
   }
 
-  private log(content) {
+  private log(content: string): void {
     if (this.#debug) {
       console.log(`[Debug] - ${content}`);
     }
   }
 
-  public async init(): Promise<void>{
+  public async init(): Promise<void> {
     this.log('Opening browser');
     await this.#browser.open();
     this.log('Browser opened');
   }
 
-  public async login(): Promise<void>{
+  public async login(): Promise<void> {
     this.log('Logging in');
     await this.#browser.page.goto(`https://github.com/${this.#GIT_USER}`);
     await this.#browser.page.click('.HeaderMenu-link--sign-in');
@@ -61,48 +61,51 @@ export class GithubClient {
     this.log('Logged in');
   }
 
-public async createBlankRepo(): Promise<void>{
+  public async createBlankRepo(): Promise<void> {
     await this.#browser.page.goto(`https://github.com/kylewandishin?tab=repositories`);
     const repoExists = await this.#browser.page.$('a[href="/kylewandishin/h1F4i4e8A4"]');
     if (repoExists) {
-        this.log('Repo already exists');
-    }else{
-    this.log('Creating blank repo');
-    await this.#browser.page.click('.text-center');
-    
-    await sleep(1000);
+      this.log('Repo already exists');
+    } else {
+      this.log('Creating blank repo');
+      await this.#browser.page.click('.text-center');
 
-    await this.#browser.page.click('div.hnmzXm > div:nth-child(1) input');
-    await this.#browser.page.fill('body > div.logged-in.env-production.page-responsive > div.application-main > main > react-app > div > form input', 'h1F4i4e8A4');
-    
-    await sleep(1000);
-    
-    await this.#browser.page.click('div.Box-sc-g0xbh4-0:nth-child(6) button');
+      await sleep(1000);
 
-    this.log(`Blank repo created - name h1F4i4e8A4`);
+      await this.#browser.page.click('div.hnmzXm > div:nth-child(1) input');
+      await this.#browser.page.fill(
+        'body > div.logged-in.env-production.page-responsive > div.application-main > main > react-app > div > form input',
+        'h1F4i4e8A4'
+      );
+
+      await sleep(1000);
+
+      await this.#browser.page.click('div.Box-sc-g0xbh4-0:nth-child(6) button');
+
+      this.log(`Blank repo created - name h1F4i4e8A4`);
     }
-}
+  }
 
-public async deleteRepo(): Promise<void>{
+  public async deleteRepo(): Promise<void> {
     this.log('Deleting repo');
     await this.#browser.page.goto(`https://github.com/${this.#GIT_USER}/h1F4i4e8A4/settings`);
     await this.#browser.page.click('#dialog-show-repo-delete-menu-dialog > span > span');
-    await this.#browser.page.click('#repo-delete-proceed-button')
-    await this.#browser.page.click('#repo-delete-proceed-button')
+    await this.#browser.page.click('#repo-delete-proceed-button');
+    await this.#browser.page.click('#repo-delete-proceed-button');
     await this.#browser.page.fill('.FormControl-input-wrap input', `${this.#GIT_USER}/h1F4i4e8A4`);
     await this.#browser.page.click('#repo-delete-proceed-button');
     this.log('Deleted repo');
-}
+  }
 
-public async runAll(): Promise<void>{
+  public async runAll(): Promise<void> {
     await new Promise((resolve) => setTimeout(resolve, 50000));
-}
+  }
 
-  public async open(): Promise<void>{
+  public async open(): Promise<void> {
     await this.#browser.open();
   }
 
-  public async close(): Promise<void>{
+  public async close(): Promise<void> {
     await this.#browser.close();
   }
 }
